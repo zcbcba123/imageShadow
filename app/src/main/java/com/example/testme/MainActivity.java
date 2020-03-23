a4ff9db0497d4564ce3f1bc302071582cbec3dc7
lvcha <997027407@qq.com>
2020-03-23 10:41:52 +0800

    first

---
+++ app/src/main/java/com/example/testme/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.testme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
